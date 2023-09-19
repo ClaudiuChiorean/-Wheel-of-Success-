64e9f270d8ae32ccddc1dbf94591b7615eb98e96
ClaudiuChiorean <claudiuchiorean2@gmail.com>
2023-09-19 16:36:26 +0300

    app.js updated

--- app.js
+++ app.js
@@ -1,172 +1,142 @@
+// Get HTML elements
 const keyboard = document.getElementById('qwerty');
 const phraseDisplay = document.getElementById('phrase');
-const startButton = document.querySelector('.btn__reset');
-
-
-/*Create a missed variable, initialized to 0, that you’ll use later to keep track of the
-number of guesses the player has missed (remember, if the player guesses
-  wrong 5 times, they lose the game) */
-
 let missedGuesses = 0;
 
+// Hide start button
+const startButton = document.querySelector('.btn__reset');
+const overlay = document.getElementById('overlay');
 
-//Create an array named phrases.
+// Array of phrases
 const phrases = [
-  "This is a sample phrase",
-  "Another example here",
-  "A third phrase with spaces",
-  "Hello World",
-  "Coding is fun"
+    "This is a sample phrase",
+    "Another example here",
+    "A third phrase with spaces",
+    "Hello World",
+    "Coding is fun"
 ];
 
 
-
-
-//Attach an event listener to the “Start Game” button to hide the start
+// Event listener for the "Start Game" button
 startButton.addEventListener('click', () => {
-  const overlay = document.getElementById('overlay');
-  overlay.style.display = 'none';
+    overlay.style.display = 'none'; // Hide the start overlay
+    addPhraseToDisplay(phraseArray);
+});
+
+
+// Function to get a random phrase as an array of characters
+const getRandomPhraseAsArray = arr => {
+    let randomIndex = Math.floor(Math.random() * arr.length);
+    let randomPhrase = arr[randomIndex];
+    return randomPhrase.split(''); // Return the selected phrase as an array of characters
+};                                 //Split the phrase into an array of characters
+
+
+
+//Function to set up the game display
+const addPhraseToDisplay = arr => {
+    const ul = phrase.querySelector('ul');
+    ul.innerHTML = '';
+
+
+    for (let i = 0; i < arr.length; i++) {
+        const li = document.createElement('li');
+        li.textContent = arr[i];
+
+        if (arr[i] !== ' ') {
+            li.classList.add('letter'); // Add 'letter' class to letters
+        } else {
+            li.classList.add('space'); // Add 'space' class to spaces
+        }
+
+        ul.appendChild(li);
+    }
+}
+
+// Get a random phrase and start the game
+const phraseArray = getRandomPhraseAsArray(phrases);
+console.log(phraseArray);
+
+
+//Function to check if a letter is in the phrase
+
+const checkLetter = button => {
+    const phraseLetters = document.querySelectorAll('.letter') // Store all of the li elements -created in addPhraseToDisplay
+    let match = null;
+
+    phraseLetters.forEach((letter) => {
+        if (letter.textContent.toLowerCase() === button.textContent.toLowerCase()) {
+          letter.classList.add('show'); // Show the letter
+          match = letter.textContent;
+        }
+      });
+
+    // for (let i = 0; i < phraseLetters.length; i++) {
+    //     if (phraseLetters[i].textContent.toLowerCase() === button.textContent.toLowerCase()) {
+    //         phraseLetters[i].classList.add('show');
+    //         match = button.textContent;
+    //     }
+    // }
+
+    return match;
+
+}
+
+//Event listener for keyboard clicks
+
+keyboard.addEventListener('click', (e) => {
+    if (e.target.tagName === 'BUTTON' && !e.target.classList.contains('chosen')) {
+        const button = e.target;
+        button.classList.add('chosen'); // Mark the chosen letter
+        button.disabled = true; // Disable the clicked button
+
+        const letterGuessed = checkLetter(button);
+
+        if (!letterGuessed) {
+            const tries = document.querySelectorAll('.tries');
+            const lostHeart = document.createElement('img');
+            lostHeart.src = 'images/lostHeart.png';
+            tries[missedGuesses].innerHTML = '';
+            tries[missedGuesses].appendChild(lostHeart);
+            missedGuesses++; // Increase missed count and display a lost heart
+        }
+        checkWin();// Check if the game is won or lost
+    }
+
 })
 
+// check if the game has been won or lost
+const checkWin = () => {
+    const letterElements = document.querySelectorAll('.letter');
+    const showElements = document.querySelectorAll('.show');
 
-// Create a getRandomPhraseAsArray function.
-// Purpose: Purpose: This function returns a random phrase from the phrases array as an array of characters.
+    if (letterElements.length === showElements.length) {
 
-
-function getRandomPhraseAsArray(phrasesArray) {
-  // Create a variable to store a random number based on the array length
-  let randomIndex = Math.floor(Math.random() * phrasesArray.length);
-
-  // Use the randomIndex to select a phrase from the array
-  let randomPhrase = phrasesArray[randomIndex];
-
-  // Return the selected phrase as an array of characters
-  return randomPhrase.split('');// Split the phrase into an array of characters
-}
-
-const randomPhraseArray = getRandomPhraseAsArray(phrases);
-console.log(randomPhraseArray);
-
-/* Create an addPhraseToDisplay function that loops through an array of
-characters. You will need to write it so that it can take any array of letters and add
-it to the display.*/
-
-function addPhraseToDisplay(phraseArray) {
-  // Select the UL element where the phrase will be displayed
-  const phraseList = document.querySelector('#phrase ul');
-
-  // Loop through the characters in the array
-  for (let i = 0; i < phraseArray.length; i++) {
-    let li = document.createElement('li');
-    li.textContent = phraseArray[i];
+        overlay.className = 'win';
+        overlay.style.display = 'flex';
+        overlay.querySelector('.title').textContent = 'Congratulations! You won!';
+        startButton.textContent = 'Play Again';
 
 
-    // Check if the character is a letter or a space and add the appropriate class
-    if (phraseArray[i] !== ' ') {
-      li.className = 'letter';
-    } else {
-      li.className = 'space';
+    } else if (missedGuesses >= 5) {
+        overlay.className = 'lose';
+        overlay.style.display = 'flex';
+        overlay.querySelector('.title').textContent = 'Sorry, You Lose. Try Again!';
+        startButton.textContent = 'Try Again';
     }
-
-    // Append the list item to the UL element
-    phraseList.appendChild(li);
-  }
-
 }
 
 
 
-// Create an addPhraseToDisplay function that loops through an array of characters
-
-function checkLetter(button) {
-  const phraseLetters = document.querySelectorAll('.letter');
-  let match = null;
-  for (let i = 0; i < phraseLetters.length; i++) {
-   
-    if (button.textContent.toLowerCase() === phraseLetters[i].textContent.toLowerCase()) {
-      phraseLetters[i].classList.add('show');
-      match = button.textContent;
-    }
-  }
-  return match;
-}
 
 
 
-addPhraseToDisplay(randomPhraseArray);
 
 
-//removeHeartImage function
 
-function removeHeartImage() {
-  let scoreboard = document.getElementById('scoreboard');
-  let heartImage = scoreboard.querySelectorAll('img[src="images/liveHeart.png"]');
-  if (heartImage.length > 0) {
-    let firstHeartImage = heartImage[0];
-    firstHeartImage.parentNode.remove();
-  }
 
-}
 
-// incrementMissedCounter function
 
-function incrementMissedCounter() {
-  missedGuesses += 1;
-
-}
-
-// Add an event listener to the keyboard
-
-qwerty.addEventListener('click', (e) => {
-  
-
- // Check if the click happened on a button element and if it doesn't have the "chosen" class
-  if (e.target.tagName === 'BUTTON' && !e.target.classList.contains('chosen')) {
-    // Add the "chosen" class to the clicked button
-    e.target.classList.add('chosen');
-
-// Call the checkLetter function and store the result in a variable
-    const letterFound = checkLetter(e.target);
-
-// Check if the letterFound is null (no match found in the phrase)
-    if (!letterFound) {
-      const tries = document.querySelectorAll('.tries');
-      tries[missedGuesses].firstElementChild.src = 'images/lostHeart.png';
-      missedGuesses++;
-      checkWin();
-    }
-  }
-  
-})
-
-// checkWin function
-
-function checkWin() {
-  const letterElements = document.querySelectorAll('.letter');
-  const showElements = document.querySelectorAll('.show');
-
-  //Check if the length of the 2 variables are the same. If they are, display the win overlay
-
-  if (letterElements.length === showElements.lenght) {
-   // Create the win overlay by adding the “win” class to the start overlay.
-      const overlay = document.getElementsByClassName('start');
-      overlay.classList.add('win');
-
-     // Change the headline text of the start overlay to show a person won.
-      overlay.document.getElementsByClassName('title').textContent = 'Congratulations! You won!';
-     //Change the display property of the overlay to “flex”
-     overlay.style.display = 'flex';
-
-    // Check if the missed counter is greater than 4. If they are, display the lose overlay
-    if (missedGuesses >= 5) {
-      overlay.classList.add('lose');
-      overlay.document.getElementsByClassName('title').textContent = 'Sorry, you lost. Try again!';
-      overlay.style.display = 'flex';
-    }
-
-  }
-
-}
 
 
 
